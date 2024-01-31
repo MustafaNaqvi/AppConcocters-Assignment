b4cc16aa965a7bbbac3a15108d56f23c8dcb69d7
Mustafa Naqvi <mustafa-naqvi@outlook.com>
2024-01-31 09:31:29 +0500

    Made Mana System script
    Signed-off-by: Mustafa Naqvi <mustafa-naqvi@outlook.com>

---
+++ Assets/Scripts/PlayerMana.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace MustafaNaqvi
+{
+    public class PlayerMana : MonoBehaviour
+    {
+        [SerializeField] private PlayerController playerController;
+        [SerializeField] private float manaRegenerationTime;
+        [SerializeField] private float manaMaxValue;
+
+        internal float Mana;
+
+        private void Start()
+        {
+            if (ReferenceEquals(playerController, null) && TryGetComponent<PlayerController>(out var pc))
+                playerController = pc;
+        }
+
+        private void Update()
+        {
+            HandleManaRegeneration();
+        }
+
+        private void HandleManaRegeneration()
+        {
+            if (Mana >= manaMaxValue)
+            {
+                Mana = manaMaxValue;
+                return;
+            }
+
+            Mana = Mathf.MoveTowards(Mana, manaMaxValue, Time.deltaTime * manaRegenerationTime);
+        }
+
+        private void ConsumeMana(float value) => Mana -= value;
+
+        private void ResetMana() => Mana = 0f;
+    }
+}
