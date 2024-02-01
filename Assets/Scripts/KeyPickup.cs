fb5e0c0684e449fcf4e857e4cac6f75d0e0f2090
Mustafa Naqvi <mustafa-naqvi@outlook.com>
2024-02-01 08:40:45 +0500

    Made KeyPickup script
    Signed-off-by: Mustafa Naqvi <mustafa-naqvi@outlook.com>

---
+++ Assets/Scripts/KeyPickup.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace MustafaNaqvi
+{
+    public class KeyPickup : CollectableObject
+    {
+        [SerializeField] private List<Transform> repositionPoints = new();
+
+        public static Action KeyCollected;
+
+        private void Start()
+        {
+            if (repositionPoints.Count > 0)
+                transform.position = repositionPoints[Random.Range(0, repositionPoints.Count)].position;
+            Spawn();
+        }
+
+        protected override void Collect(GameObject collectingObject)
+        {
+            if (ReferenceEquals(SpawnedObject, null)) return;
+            KeyCollected?.Invoke();
+            Destroy(SpawnedObject);
+        }
+    }
+}
